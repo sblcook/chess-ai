e6dab6d3f6b97830452c6c9115e6e241d03b9c3f
Sam Cook <samuel.cook@temple.edu>
2017-11-04 19:11:08 -0400

    Added enum for piece types, added field on piece for piece type

--- src/main/Pieces/Piece.java
+++ src/main/Pieces/Piece.java
@@ -2,19 +2,26 @@ package Pieces;
 
 import Board.Tile;
 import Enums.Colors;
+import Enums.PieceType;
 
 class Piece {
 
     private Colors pieceColor;
     private Tile location;
+    private PieceType pieceType;
 
-    Piece(Colors color, Tile tile){
+    Piece(Colors color, Tile tile, PieceType pieceType){
         this.pieceColor = color;
         this.location = tile;
+        this.pieceType = pieceType;
     }
 
     Colors getPieceColor(){
         return this.pieceColor;
+    }
+
+    PieceType getPieceType(){
+        return this.pieceType;
     }
 
     Tile getLocation(){
--- src/test/Pieces/PieceTest.java
+++ src/test/Pieces/PieceTest.java
@@ -2,6 +2,7 @@ package Pieces;
 
 import Board.Tile;
 import Enums.Colors;
+import Enums.PieceType;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -9,7 +10,7 @@ import static junit.framework.TestCase.assertEquals;
 public class PieceTest {
 
     Tile tile = new Tile(2, 4);
-    Piece piece = new Piece(Colors.BLACK, null);
+    Piece piece = new Piece(Colors.BLACK, null, PieceType.KING); //tile null to be set in getter test
 
     @Test
     public void getPieceColorTest() throws Exception {
@@ -20,6 +21,19 @@ public class PieceTest {
     public void getLocationTest() throws Exception {
         piece.setLocation(tile);
         assertEquals(tile, piece.getLocation());
+    }
+
+    @Test
+    public void getPieceTypeTest() throws Exception {
+        assertEquals(PieceType.KING, piece.getPieceType());
+    }
+
+    @Test
+    public void pieceTest() throws Exception{
+        Piece constructorPiece = new Piece(Colors.WHITE, tile, PieceType.QUEEN);
+        assertEquals(Colors.WHITE, constructorPiece.getPieceColor());
+        assertEquals(tile, constructorPiece.getLocation());
+        assertEquals(PieceType.QUEEN, constructorPiece.getPieceType());
     }
 
 }
