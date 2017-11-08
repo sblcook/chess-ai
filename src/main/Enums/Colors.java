e782fd537e23996f3b86fe2b8f0d50bf64267247
Sam Cook <samuel.cook@temple.edu>
2017-11-07 21:47:57 -0500

    Updated Colors enum

--- src/main/Enums/Colors.java
+++ src/main/Enums/Colors.java
@@ -1,13 +1,5 @@
 package Enums;
 
 public enum Colors {
-    BLACK("BLACK"),
-    WHITE("WHITE");
-
-    private final String color;
-
-    Colors(String color){
-        this.color = color;
-    }
-
+    BLACK, WHITE
 }
