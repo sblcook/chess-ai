32e3ea35375c67e33d28ba4c3207230e8e65a8ee
Chris <ceaustin117@gmail.com>
2017-11-10 03:38:51 -0800

    Added Main w/ hello world message

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args )
+    {
+        System.out.println("Hello World ");
+    }
+    public String testingMethod(){
+        return "test string";
+    }
+}
