7a100a2250987a82d6175a8ed8ab20a9fe08ba51
Jessika <jessika.murphy@temple.edu>
2017-11-21 19:29:30 -0500

    committing player interface and human player implementation

--- src/main/Player/Player.java
+++ src/main/Player/Player.java
@@ -1,6 +1,30 @@
 
+package Player;
+
+import Enums.Result;
+
+import java.util.Map;
 
 public abstract class Player {
 
-    public abstract ChessPieceTurn getTurn(Tile p);
+    private String name;
+    private Map<Result, Integer> Performance;
+
+    public Player(String name, Map<Result, Integer> performance) {
+        this.name = name;
+        Performance = performance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public Map<Result, Integer> getPerformance() {
+        return Performance;
+    }
+
+    public void setPerformance(Map<Result, Integer> performance) {
+        Performance = performance;
+    }
 }
