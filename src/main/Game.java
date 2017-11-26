ae207fc8a35b52eb404ce254d375c26c422b6e7f
Chris <ceaustin117@gmail.com>
2017-11-26 14:57:22 -0800

    Game Class Tests Added

--- src/main/Game.java
+++ src/main/Game.java
@@ -11,6 +11,7 @@ import Player.Player;
 import Player.HumanPlayer;
 
 public class Game {
+
     private Board gameBoard;
     private Map<Player, List<Move> >  playedMoves;
     private Player turn;
@@ -26,6 +27,62 @@ public class Game {
         playerColor = new HashMap<Player, Color>();
         result = new HashMap<Player, Result>();
     }
+    public Board getGameBoard() {
+        return gameBoard;
+    }
+
+    public Map<Player, List<Move>> getPlayedMoves() {
+        return playedMoves;
+    }
+
+    public Player getTurn() {
+        return turn;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public Map<Player, Color> getPlayerColor() {
+        return playerColor;
+    }
+
+    public Map<Player, Result> getResult() {
+        return result;
+    }
+
+    public void setGameBoard(Board gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public void setPlayedMoves(Map<Player, List<Move>> playedMoves) {
+        this.playedMoves = playedMoves;
+    }
+
+    public void setTurn(Player turn) {
+        this.turn = turn;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public void setPlayerColor(Map<Player, Color> playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    public void setResult(Map<Player, Result> result) {
+        this.result = result;
+    }
+
     private void startGame()
     {
 
--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,2 +1,70 @@
+import Board.Board;
+import Enums.Result;
+import Player.Player;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+    private Board gameBoard;
+    private Map<Player, List<Move> > playedMoves;
+    private Player turn;
+    private Player player1;
+    private Player player2;
+    private Map<Player, Color> playerColor;
+    private Map<Player, Result> result;
+
+    @Test
+    public void boardTest() throws Exception {
+        Game newGame = new Game();
+        newGame.setGameBoard(gameBoard);
+        assertEquals(gameBoard,newGame.getGameBoard());
+    }
+
+    @Test
+    public void playedMovesTest() throws Exception {
+        Game newGame = new Game();
+        newGame.setPlayedMoves(playedMoves);
+        assertEquals(playedMoves,newGame.getPlayedMoves());
+    }
+
+    @Test
+    public void turnTest() throws Exception {
+        Game newGame = new Game();
+        newGame.setTurn(turn);
+        assertEquals(turn,newGame.getTurn());
+    }
+
+    @Test
+    public void player1Test() throws Exception {
+        Game newGame = new Game();
+        newGame.setPlayer1(player1);
+        assertEquals(player1,newGame.getPlayer1());
+    }
+
+    @Test
+    public void player2Test() throws Exception {
+        Game newGame = new Game();
+        newGame.setPlayer2(player2);
+        assertEquals(player2,newGame.getPlayer2());
+    }
+
+    @Test
+    public void playerColorTest() throws Exception {
+        Game newGame = new Game();
+        newGame.setPlayerColor(playerColor);
+        assertEquals(playerColor,newGame.getPlayerColor());
+    }
+
+    @Test
+    public void resultTest() throws Exception {
+        Game newGame = new Game();
+        newGame.setResult(result);
+        assertEquals(result,newGame.getResult());
+    }
+
 }
